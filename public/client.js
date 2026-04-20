8c720bd9dc849bf31b22486f74fe43bc9724b3a2
Yiğit Aksoy <yigitaksoyyyyy@gmail.com>
2026-04-20 13:55:28 +0300

    live

--- public/client.js
+++ public/client.js
@@ -77,7 +77,7 @@ if (Hls.isSupported() && p2pml.hlsjs.Engine.isSupported()) {
     p2pml.hlsjs.initHlsJsPlayer(hls);
     
     // Sağlam Test Yayını URL'si (İstersen burayı kendi lokal 'public/video/...' dosyanla değiştirebilirsin)
-    hls.loadSource('https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
+    hls.loadSource('https://canal.mediaserver.com.co/live/buenisimatv.m3u8');
     hls.attachMedia(video);
 
     // Hata Kurtarma Algoritması
